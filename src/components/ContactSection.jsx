4f910b0575ff2483ac241df4f03932e149b88b58
okjordanwood <jordancwood2002@gmail.com>
2025-05-31 22:41:19 -0400

    Add .env

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -19,13 +19,13 @@ const ContactSection = () => {
 
     emailjs
       .sendForm(
-        "your_service_id",
-        "your_template_id",
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         form.current,
-        "your_public_key"
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
-        (result) => {
+        () => {
           setSubmitted(true);
           form.current.reset();
           recaptchaRef.current.reset();
@@ -45,16 +45,11 @@ const ContactSection = () => {
       </div>
 
       <form ref={form} onSubmit={sendEmail} className="contact-form">
-        <input type="text" name="user_name" placeholder="Your Name" required />
-        <input
-          type="email"
-          name="user_email"
-          placeholder="Your Email"
-          required
-        />
+        <input type="text" name="name" placeholder="Your Name" required />
+        <input type="email" name="email" placeholder="Your Email" required />
         <textarea name="message" placeholder="Your Message" rows="5" required />
         <ReCAPTCHA
-          sitekey="your_recaptcha_site_key"
+          sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
           ref={recaptchaRef}
           className="recaptcha"
         />
